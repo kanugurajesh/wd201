fdec4e7aa46b8564b2043608f4d5fa32d1efb35e
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-17 21:39:33 +0530

    Adding the todolist built using closures

---
+++ todo-cli/hello.js
@@ -0,0 +1,13 @@
+const formattedDate = d => {
+    return d.toISOString().split("T")[0]
+}
+
+var dateToday = new Date()
+const today = formattedDate(dateToday)
+const yesterday = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() - 1))
+)
+
+console.log(yesterday)
+console.log(today)
+console.log(yesterday > today)
